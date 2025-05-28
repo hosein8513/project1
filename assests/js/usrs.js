aa82b1d23282cc77753d85ebad0d6e2eb80ce748
hosein <kazmyh279@gmail.com>
2025-05-28 07:59:05 -0700

    step10

--- assests/js/usrs.js
+++ assests/js/usrs.js
@@ -1,4 +1,21 @@
  document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.collapsible');
     var instances = M.Collapsible.init(elems, {});
-  });
+ 
+const adduserbutton = document.getElementById('adduserbutton')
+adduserbutton.addEventListener('click' , ()=>{document.querySelector('.add-user-modal').classList.add
+  ('show')
+  document.querySelector('.add-user-modal-back').classList.remove
+  ('dis-none')
+})
+
+
+document.querySelector('.add-user-modal-back').addEventListener('click', (e)=>{document.querySelector
+  ('.add-user-modal').classList.remove('show')
+  e.target.classList.add('dis-none')
+})
+
+
+
+
+ });
