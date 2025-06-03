27f8836b5ab44e81a95d83a95d48f2f43f0f390f
hosein <kazmyh279@gmail.com>
2025-06-03 19:13:50 +0330

    step13

--- assests/js/alpinejscontroller/maincontroler.js
+++ assests/js/alpinejscontroller/maincontroler.js
@@ -1,15 +1,44 @@
 document.addEventListener('alpine:init', () => {
-    Alpine.data('userdata', function(){
-      return{  users: [],
+   Alpine.data('userdata', function() {
+    return {
+        users: [],
+        pageusers: [],
         isloading: false,
+        showaddmodal: false,
+        pagecount: 1,
+        itemscount: 4,
+        currentpage: 1,
         getusers() {
             this.isloading = true
             axios.get("https://jsonplaceholder.typicode.com/users")
                 .then((res) => {
                     this.users = res.data;
-                    this.isloading = false
+                    this.pagination(); // الان شناخته می‌شود
                     console.log(res);
-                });
+                })
+                .finally(() => {
+                    this.isloading = false
+                })
+        },
+        pagination() {
+            this.pagecount = Math.ceil(this.users.length / this.itemscount);
+            let start = (this.currentpage * this.itemscount) - this.itemscount;
+            let end = this.currentpage * this.itemscount;
+            this.pageusers = this.users.slice(start, end);
+        },
+        nextpage(){
+            this.currentpage++
+            if (this.currentpage > this.pagecount){
+                this.currentpage = this.pagecount
+            }
+         this.pagination()   
+        },
+        prevpage(){
+            this.currentpage--
+            if (this.currentpage < 1)this.currentpage = 1
+            this.pagination()
         }
-   } });
-});
+
+        
+    }
+})})
--- assests/js/usrs.js
+++ assests/js/usrs.js
@@ -2,20 +2,21 @@
     var elems = document.querySelectorAll('.collapsible');
     var instances = M.Collapsible.init(elems, {});
  
-const adduserbutton = document.getElementById('adduserbutton')
-adduserbutton.addEventListener('click' , ()=>{document.querySelector('.add-user-modal').classList.add
-  ('show')
-  document.querySelector('.add-user-modal-back').classList.remove
-  ('dis-none')
-})
+// const adduserbutton = document.getElementById('adduserbutton')
+// adduserbutton.addEventListener('click' , ()=>{document.querySelector('.add-user-modal').classList.add
+//   ('show')
+//   document.querySelector('.add-user-modal-back').classList.remove
+//   ('dis-none')
+// })
 
 
-document.querySelector('.add-user-modal-back').addEventListener('click', (e)=>{document.querySelector
-  ('.add-user-modal').classList.remove('show')
-  e.target.classList.add('dis-none')
-})
+// document.querySelector('.add-user-modal-back').addEventListener('click', (e)=>{document.querySelector
+//   ('.add-user-modal').classList.remove('show')
+//   e.target.classList.add('dis-none')
+// })
+
+//  });   
 
 
 
-
- });
+ })
